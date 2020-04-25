1e1a51eae126c45b0b1d0c90759d54cfe11d4ba4
mrning <mrnzc@foxmail.com>
2020-04-25 11:27:13 +0800

    initial

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QtNetwork>
+#include<QPushButton>
+#include<QDebug>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
+    connect(ui->sendButton,&QPushButton::clicked,[=](){
+        qDebug()<<"测试";
+    });
 
 }
 
