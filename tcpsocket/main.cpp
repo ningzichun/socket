048bae834f85abd5bea110817d2236ab50475fc8
mrning <mrnzc@foxmail.com>
2020-04-27 12:30:57 +0800

    添加资源文件

--- tcpsocket/main.cpp
+++ tcpsocket/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
-    w.setWindowIcon(QIcon("../zhuzi.png"));
+    w.setWindowIcon(QIcon(":/res/zhuzi.png"));
     w.show();
     return a.exec();
 }
--- tcpsocket/tcp.pro
+++ tcpsocket/tcp.pro
@@ -41,3 +41,6 @@ else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
 
 DISTFILES +=
+
+RESOURCES += \
+    res.qrc
