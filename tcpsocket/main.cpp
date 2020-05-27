e1b97354dc5495de9833c29259ad5097760f44d4
Anadem <81154420@qq.com>
2020-05-27 22:15:48 +0800

    窗口自适应高分辨率显示器

--- tcpsocket/main.cpp
+++ tcpsocket/main.cpp
@@ -3,6 +3,9 @@
 #include "mainwindow.h"
 
 int main(int argc, char *argv[]) {
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0))
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowIcon(QIcon(":/res/zhuzi.png"));
--- tcpsocket/mainwindow.h
+++ tcpsocket/mainwindow.h
@@ -38,40 +38,29 @@ signals:
     //void closepaint(bool);
     //void transportpaint(QString);
    private slots:
+    /*按钮事件*/
     void on_actionGetIP_triggered();
-
     void on_actionAbout_triggered();
-
     void on_connectButton_clicked();
-
     void on_startButton_clicked();
-
     void on_sendButton_clicked();
-
     void on_fileButton_clicked();
-
     void on_fileopenButton_clicked();
-
     void on_actionsettings_triggered();
-
     void on_actioninfo_triggered();
-
     void on_imgButton_clicked();
-
-
+    /*按钮事件*/
+    /*音频*/
     void onReadyRead();
     void readyReadSlot();
-
+    /*音频*/
     /*画图*/
     void changemode();
-
     void changepaintpic(QString);
-
     void on_paintsendButton_clicked();
-
     void on_paintreceiveButton_clicked();
-
     void on_pushButton_clicked();
+    /*画图*/
 
 private:
     void readData(QTcpSocket* targetSocket,QByteArray& array);
@@ -84,16 +73,12 @@ private:
     QFile* receivingFile;
 
     /*音频*/
-
     QUdpSocket *udpServer;
     QUdpSocket *udpSocket;//用于音频信号的发送
-
     QAudioInput *input;
     QIODevice *inputDevice;
     QAudioOutput *output;
     QIODevice *outputDevice;
-
-
     struct video{
         char data[1024];
         int lens;
